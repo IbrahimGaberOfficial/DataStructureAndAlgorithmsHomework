cbaaef0cafacf8b0f138ff30b739dc72a2dae504
Ibrahim Gaber <ibrahim.gaber.official@gmail.com>
2023-09-01 01:53:13 +0300

    solve more on trie/HomeWork2

--- 10_Trie/HomeWork2/02_problem2.cpp
+++ 10_Trie/HomeWork2/02_problem2.cpp
@@ -4,9 +4,9 @@ using namespace std;
 class trie{
 private:
     static const int MAX_CHAR = 26;
-    trie *child[MAX_CHAR];
     bool is_leaf{ };
     char from{};
+    trie *child[MAX_CHAR];
 
 public:
     trie(char from_init) : from(from_init){
@@ -50,40 +50,70 @@ public:
         _get_all_strings(str, now);
     }
     void _get_all_strings(vector<string> &str, trie* now, string collected = ""){
+        if(now->is_leaf)
+            str.push_back(collected);
         for(int i = 0; i < MAX_CHAR; i++){
             if(now->child[i] != nullptr){
                 // cout << char('a' + i);
-                collected += char('a' + i);
-                if(now->child[i]->is_leaf){
-                    str.push_back(collected);
-                    // cout << " ";
-                    // str.push_back(collected);
-                    
-                }
-                _get_all_strings(str, now->child[i], collected);
-                // cout << "\n";
-                collected = "";
+               
+                _get_all_strings(str, now->child[i], collected + char('a' + i) );
+
             }
             
             
         }
+    }
+    void auto_complete(string str, vector<string> &result, int idx = 0){
+       trie* now = this;
+       for(int i = 0; i < str.size(); i++){
+        int current = str[i] - 'a';
+
+        if(now->child[current] == nullptr)
+            return;
+        now = now->child[current];
+       }
+    //    if(now->is_leaf){
+    //         result.push_back(str);
+    //    }
+       _get_all_strings(result, now);
+       
     }
 };
 
 int main(){
     trie* myTrie = new trie('#');
     myTrie->insert("abcd", 0);
-    myTrie->insert("xyz", 0);
-    myTrie->insert("a", 0);
     myTrie->insert("ab", 0);
+    myTrie->insert("a", 0);
+    myTrie->insert("abx", 0);
+    myTrie->insert("abyz", 0);
+    myTrie->insert("xyz", 0);
     myTrie->insert("xyzw", 0);
     myTrie->insert("bcd", 0);
+    myTrie->insert("bxd", 0);
+    myTrie->insert("bid", 0);
+
    
 
     vector<string> res;
+    string querey = "b";
     myTrie->get_all_strings(res);
     for(int i = 0; i < res.size(); i++){
-        cout << res[i] << "\n";
+        cout << res[i] << " ";
+    }
+    cout << "\n------------------" << endl;
+
+    vector<string> new_res;
+    myTrie->auto_complete(querey, new_res);
+
+   cout << "\n------------------" << endl;
+
+    for(int i = 0; i < new_res.size(); i++){
+        // if(!i){
+        //     cout << new_res[i] << endl;
+        //     continue;
+        // }
+        cout <<  querey +  new_res[i] << "\n";
     }
 return 0;
 }
